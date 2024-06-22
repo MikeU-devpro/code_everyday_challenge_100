189967e9f61de68c9c80b9d407827661b672238b
Ngaleu Michael <ngaleumike1234@outlook.com>
2024-06-22 16:57:11 +0100

    completing the tutorial version of project 4

--- Day 4 - Hidden Search Widget/script.js
+++ Day 4 - Hidden Search Widget/script.js
@@ -1,6 +1,8 @@
-const search = document.querySelector('img')
-const input = document.querySelector('input')
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
 
-search.addEventListener('click', () => {
-    input.classList.toggle('hide')
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
 })
